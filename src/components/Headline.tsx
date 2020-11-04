72aca3f7c8cc8cd1a01144f8eeeb1b8dfb8ea59f
Alex-La <lars6162@gmail.com>
2020-11-05 02:43:20 +0300

    Headline

--- src/components/Headline.tsx
+++ src/components/Headline.tsx
@@ -1,9 +1,72 @@
 import React, { Component } from "react";
+import "../styles/Headline.css";
+import { Container, Card, Row, Col, Button } from "react-bootstrap";
+import ImageObject from "../classes/ImageObject";
+import ArrowRight from "bootstrap-icons/icons/arrow-right.svg";
 
-interface Props {}
+interface Props {
+  backgroundColor?: string;
+  textColor?: string;
+  firstImage?: ImageObject;
+  secondImage?: ImageObject;
+  tagline?: string;
+  clarification?: string;
+  linkBtnText?: string;
+  btnVariant?: string;
+}
 
 interface State {}
 
-class Headline extends Component<Props, State> {}
+class Headline extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Container className="block-cont">
+        <Card
+          className={`${this.props.backgroundColor} ${this.props.textColor}`}
+        >
+          <Row>
+            {this.props.firstImage && (
+              <Col>
+                <Card.Img
+                  width={this.props.firstImage.width}
+                  height={this.props.firstImage.height}
+                  src={this.props.firstImage.data}
+                  alt={this.props.firstImage.data}
+                  className="hl-img"
+                />
+              </Col>
+            )}
+
+            {this.props.secondImage && (
+              <Col>
+                <Card.Img
+                  width={this.props.secondImage.width}
+                  height={this.props.secondImage.height}
+                  src={this.props.secondImage.data}
+                  alt={this.props.secondImage.data}
+                  className="hl-img"
+                />
+              </Col>
+            )}
+          </Row>
+          <Card.ImgOverlay>
+            <Card.Title>{this.props.tagline}</Card.Title>
+            <Card.Text>{this.props.clarification}</Card.Text>
+            {this.props.linkBtnText && (
+              <Button size="sm" variant={this.props.btnVariant}>
+                {this.props.linkBtnText}
+                <img src={ArrowRight} className="hl-btn-icon" />
+              </Button>
+            )}
+          </Card.ImgOverlay>
+        </Card>
+      </Container>
+    );
+  }
+}
 
 export default Headline;
--- src/system/App.tsx
+++ src/system/App.tsx
@@ -1,17 +1,12 @@
 import React from "react";
 import "../styles/App.css";
 
-import Picture from "../components/Picture";
+import Headline from "../components/Headline";
 
 function App() {
   return (
     <div className="App">
-      <Picture
-        title="heelo"
-        subTitle="gerhe"
-        pictureURL="https://www.frogx3.com/wp-content/uploads/2017/12/Undraw-ilustraciones-1.png"
-        aspectRatio="16by9"
-      />
+      <Headline tagline="hello" clarification="world" linkBtnText="More" />
     </div>
   );
 }
